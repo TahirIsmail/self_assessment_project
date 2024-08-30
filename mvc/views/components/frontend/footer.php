b6491241efa1f99e91b001ad0d5ea0cc3f38b37c
FarhadMughal43 <farhadmughal5550@gmail.com>
2024-08-30 10:41:51 +0500

    contact-us

--- mvc/views/components/frontend/footer.php
+++ mvc/views/components/frontend/footer.php
@@ -265,7 +265,7 @@
   <div class="container-fluid text-white" style="background: #061429">
       <div class="container text-center">
           <div class="row justify-content-end">
-              <div class="col-lg-8 col-md-6">
+              <div class="col-lg-8 col-md-6 mx-auto">
                   <div
                       class="d-flex align-items-center justify-content-center"
                       style="height: 75px">
