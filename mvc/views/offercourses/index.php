d01bb446e8632aa7ed5a76e28f01ff6726260111
= <=>
2024-09-11 17:03:42 +0500

    sitaa

--- mvc/views/offercourses/index.php
+++ mvc/views/offercourses/index.php
@@ -22,53 +22,64 @@
 
 
                 <div id="hide-table">
-                    <table id="example1" class="table table-striped table-bordered table-hover dataTable no-footer">
-                        <thead>
-                            <tr>
-                                <th class="col-lg-1"><?= $this->lang->line('serial_no') ?></th>
-                                <th class="col-lg-2"><?= $this->lang->line('course_id') ?></th>
-                                <th class="col-lg-2"><?= $this->lang->line('course_name') ?></th>
-                                <th class="col-lg-2"><?= $this->lang->line('course_description') ?></th>
-                                <?php if (permissionChecker('course_edit') || permissionChecker('course_delete')) { ?>
-                                    <th class="col-lg-1"><?= $this->lang->line('action') ?></th>
-                                <?php } ?>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php if (!empty($courses)) {
-                                $i = 1;
-                                foreach ($courses as $course) { ?>
-                                    <tr>
-                                        <td data-title="<?= $this->lang->line('serial_no') ?>"><?= $i; ?></td>
-                                        <td data-title="<?= $this->lang->line('course_id') ?>"><?= $course->course_id; ?></td>
-                                        <td data-title="<?= $this->lang->line('course_name') ?>"><?= $course->course_name; ?></td>
-                                        <td data-title="<?= $this->lang->line('course_description') ?>">
-                                            <?= $course->course_description; ?>
-                                        </td>
-                                        <?php if (permissionChecker('course_edit') || permissionChecker('course_delete')) { ?>
-                                            <td data-title="<?= $this->lang->line('action') ?>">
+    <table id="example1" class="table table-striped table-bordered table-hover dataTable no-footer">
+        <thead>
+            <tr>
+                <th class="col-lg-1"><?= $this->lang->line('serial_no') ?></th>
+                <th class="col-lg-2"><?= $this->lang->line('course_id') ?></th>
+                <th class="col-lg-2"><?= $this->lang->line('course_name') ?></th>
+                <th class="col-lg-2"><?= $this->lang->line('course_description') ?></th>
+                <?php if (permissionChecker('course_edit') || permissionChecker('course_delete')) { ?>
+                    <th class="col-lg-1"><?= $this->lang->line('action') ?></th>
+                <?php } ?>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if (!empty($courses)) {
+                $i = 1;
+                foreach ($courses as $course) { ?>
+                    <tr>
+                        <td data-title="<?= $this->lang->line('serial_no') ?>"><?= $i; ?></td>
+                        <td data-title="<?= $this->lang->line('course_id') ?>"><?= $course->course_id; ?></td>
+                        <td data-title="<?= $this->lang->line('course_name') ?>"><?= $course->course_name; ?></td>
+                        <td data-title="<?= $this->lang->line('course_description') ?>">
+                            <p class="short-description">
+                                <?php 
+                                
+                                $short_desc = substr($course->course_description, 0, 100);
+                                echo $short_desc;
+                                ?>
+                                <?php if (strlen($course->course_description) > 100): ?>
+                                    <span class="see-more" style="color:blue; cursor:pointer;">See More</span>
+                                <?php endif; ?>
+                            </p>
+                            <p class="full-description" style="display:none;">
+                                <?= $course->course_description; ?>
+                                <span class="see-less" style="color:blue; cursor:pointer;">See Less</span>
+                            </p>
+                        </td>
+                        <?php if (permissionChecker('course_edit') || permissionChecker('course_delete')) { ?>
+                            <td data-title="<?= $this->lang->line('action') ?>">
+                                <a href="<?= base_url('offercourses/edit/' . $course->id) ?>" class="btn btn-warning btn-sm">
+                                    <i class="fa fa-pencil"></i> <?= $this->lang->line('edit') ?>
+                                </a>
+                                <a href="<?= base_url('offercourses/delete/' . $course->id) ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this course?');">
+                                    <i class="fa fa-trash"></i> <?= $this->lang->line('delete') ?>
+                                </a>
+                            </td>
+                        <?php } ?>
+                    </tr>
+                <?php $i++;
+                }
+            } else { ?>
+                <tr>
+                    <td colspan="5"><?= $this->lang->line('no_data_available') ?></td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
 
-                                                <a href="<?= base_url('offercourses/edit/' . $course->id) ?>" class="btn btn-warning btn-sm">
-                                                    <i class="fa fa-pencil"></i> <?= $this->lang->line('edit') ?>
-                                                </a>
-
-
-                                                <a href="<?= base_url('offercourses/delete/' . $course->id) ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this course?');">
-                                                    <i class="fa fa-trash"></i> <?= $this->lang->line('delete') ?>
-                                                </a>
-                                            </td>
-                                        <?php } ?>
-                                    </tr>
-                                <?php $i++;
-                                }
-                            } else { ?>
-                                <tr>
-                                    <td colspan="5"><?= $this->lang->line('no_data_available') ?></td>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
 
             </div>
         </div>
@@ -101,3 +112,39 @@
         maximumSelectionSize: 6
     });
 </script>
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+      
+        const seeMoreElements = document.querySelectorAll('.see-more');
+        const seeLessElements = document.querySelectorAll('.see-less');
+        
+        seeMoreElements.forEach((element) => {
+            element.addEventListener('click', function() {
+                
+                const parent = this.closest('td');
+                parent.querySelector('.full-description').style.display = 'block';
+                parent.querySelector('.short-description').style.display = 'none';
+            });
+        });
+
+        seeLessElements.forEach((element) => {
+            element.addEventListener('click', function() {
+            
+                const parent = this.closest('td');
+                parent.querySelector('.full-description').style.display = 'none';
+                parent.querySelector('.short-description').style.display = 'block';
+            });
+        });
+    });
+</script>
+<style>
+.see-more, .see-less {
+    color: blue;
+    cursor: pointer;
+    text-decoration: underline;
+}
+
+.course-description p {
+    margin: 0;
+}
+</style>
